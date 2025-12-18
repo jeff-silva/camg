b7763a6c3b0451dfe2e4bcd258d1cb9e754d3fac
Jeff Silva <jeferson.i.silva@gmail.com>
2025-12-18 12:39:32 -0300

    Integrado Laravel Octane com FrankenPHP. Adicionada dependência Octane, configurado script de desenvolvimento, criado Caddyfile para o servidor e arquivo de configuração Octane. Atualizado .gitignore.

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# export SERVICE_APP_INIT="php artisan serve --host=0.0.0.0 --port=80"
 export SERVICE_APP_INIT="php artisan octane:start --server=frankenphp --host=0.0.0.0 --caddyfile=/app/Caddyfile --watch"
 # export SERVICE_APP_INIT="php artisan octane:start --server=roadrunner --host=0.0.0.0 --port=80 --rpc-port=6001 --watch"
 # export SERVICE_APP_INIT="php artisan octane:start --server=swoole --host=0.0.0.0 --port=80 --watch"
