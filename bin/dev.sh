4725713c41d5ac1ce28064260dc144064a83e1b3
Jeff Silva <jeferson.i.silva@gmail.com>
2025-12-18 12:14:05 -0300

    feat: setup ambiente de desenvolvimento e produção com Docker Compose Adiciona o boilerplate completo para um ambiente de desenvolvimento e produção Dockerizado. Inclui scripts para deploy, dev e down, e configura serviços essenciais como Nginx Proxy Manager, Keycloak, MongoDB, DBGate, Laravel (app e schedule), Minio e um frontend Node.js.

---
+++ bin/dev.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export SERVICE_APP_INIT="php artisan octane:start --server=frankenphp --host=0.0.0.0 --caddyfile=/app/Caddyfile --watch"
+# export SERVICE_APP_INIT="php artisan octane:start --server=roadrunner --host=0.0.0.0 --port=80 --rpc-port=6001 --watch"
+# export SERVICE_APP_INIT="php artisan octane:start --server=swoole --host=0.0.0.0 --port=80 --watch"
+
+docker compose up --build --force-recreate --remove-orphans
---
+++ bin/service-app-install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker compose exec -it service-app php artisan app:install
